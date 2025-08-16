4f6bf9d13247ba94ec1865c66b31667d57911dd1
Adithya Venkat Ramanan <addyvenkat@gmail.com>
2025-08-15 19:37:56 -0700

    Ignore generated outputs

--- avr_sh_builtins.d
+++
@@ -1,5 +0,0 @@
-avr_sh_builtins.o: avr_sh_builtins.c avr_sh_builtins.h avr_sh_headers.h
-
-avr_sh_builtins.h:
-
-avr_sh_headers.h:
--- avr_sh_helper.d
+++
@@ -1,5 +0,0 @@
-avr_sh_helper.o: avr_sh_helper.c avr_sh_helper.h avr_sh_headers.h
-
-avr_sh_helper.h:
-
-avr_sh_headers.h:
